dea09de4357345580ea91caf82d6e883cf0c2034
Abul Hossain <mdabulhossain780@gmail.com>
2023-03-22 01:52:38 +0600

    feat: use schema API into the custom-schema page

--- src/pages/schema/custom.tsx
+++ src/pages/schema/custom.tsx
@@ -1,60 +1,63 @@
-import { PlusCircleIcon, TrashIcon } from "@heroicons/react/24/outline";
+import {
+  TrashIcon,
+  PlusCircleIcon,
+  CheckCircleIcon,
+} from "@heroicons/react/24/outline";
+import debounce from "lodash/debounce";
+import { type ChangeEvent, useMemo } from "react";
+
+import { api } from "~/utils/api";
 import {
   NavItem,
   Navigation,
   NavItemWrapper,
 } from "~/components/Layout/Navigation";
+import { useSchemaContext } from "~/contexts";
 import { type FieldType } from "~/types/schema.types";
 import { RenderSchemaFields } from "~/components/RenderSchemaField";
 
 const Custom = () => {
-  const customSchema: FieldType = {
-    name: "Custom-Schema",
-    fields: [
-      {
-        name: "@type",
-        value: "Organization",
-      },
-      {
-        name: "ContactInfo",
-        fields: [
-          {
-            name: "@type",
-            value: "contactInfo",
-          },
-          {
-            name: "email",
-            value: "abul@dorik.io",
-          },
-          {
-            name: "name",
-            value: "abul",
-          },
-          {
-            name: "News",
-            fields: [
-              {
-                name: "@type",
-                value: "News",
-              },
-              {
-                name: "@type",
-                value: "News",
-              },
-              {
-                name: "@type",
-                value: "News",
-              },
-              {
-                name: "@type",
-                value: "News",
-              },
-            ],
-          },
-        ],
-      },
-    ],
+  const { name, schema, isLoading, setSchema, id } = useSchemaContext();
+  const { mutate: createSchema } = api.scheme.create.useMutation({
+    onSuccess: (data) => {
+      localStorage.setItem("schemaId", data?.id || "");
+    },
+  });
+
+  const { mutate: updateSchema } = api.scheme.updateSchema.useMutation();
+
+  const handleBackgroundSave = useMemo(
+    () =>
+      debounce((name: string, schema: FieldType[]) => {
+        createSchema({ name, schema: JSON.stringify(schema) });
+      }, 1000),
+    [createSchema]
+  );
+
+  const handleAddProperty = () => {
+    const payload = [...schema, { name: "", value: "" }];
+    setSchema(payload);
   };
+  const handleAddGroup = () => {
+    const payload = [...schema, { name: "", fields: [] }];
+    setSchema(payload);
+  };
+
+  const handleNameChange = useMemo(
+    () =>
+      debounce((event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        const payload = { name: value, schema: JSON.stringify(schema) };
+        if (id) {
+          return updateSchema({ ...payload, id });
+        }
+        createSchema(payload);
+      }, 500),
+    [createSchema, id, schema, updateSchema]
+  );
+
+  if (isLoading) return <p>loading...</p>;
+
   return (
     <>
       <Navigation>
@@ -68,13 +71,13 @@ const Custom = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="mr-1 h-6 w-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"
               />
             </svg>
@@ -85,18 +88,32 @@ const Custom = () => {
       <main>
         <div className="flex items-center justify-between bg-sky-300 px-4">
           <input
-            className="h-9 w-96 rounded-md border p-2 text-gray-700 "
+            defaultValue={name}
+            onChange={handleNameChange}
             placeholder="Schema Name"
-            value={customSchema.name}
+            className="h-9 w-96 rounded-md border p-2 text-gray-700 "
           />
           <NavItemWrapper>
-            <NavItem className="hover:border-b-transparent">
+            <NavItem
+              onClick={handleAddProperty}
+              className="hover:border-b-transparent"
+            >
               <PlusCircleIcon className="mr-1 h-6 w-6" />
               Add Property
             </NavItem>
-            <NavItem className="hover:border-b-transparent">
+            <NavItem
+              onClick={handleAddGroup}
+              className="hover:border-b-transparent"
+            >
               <PlusCircleIcon className="mr-1 h-6 w-6" />
               Add Group
+            </NavItem>
+            <NavItem
+              className="flex-1 justify-end"
+              onClick={() => handleBackgroundSave(name, schema)}
+            >
+              <CheckCircleIcon className="mr-1 h-6 w-6" />
+              Save
             </NavItem>
             <NavItem className="hover:border-b-transparent">
               <TrashIcon className="mr-1 h-6 w-6" />
@@ -104,7 +121,7 @@ const Custom = () => {
             </NavItem>
           </NavItemWrapper>
         </div>
-        <RenderSchemaFields fields={customSchema.fields || []} />
+        <RenderSchemaFields fields={schema || []} />
       </main>
     </>
   );
