b742d81fbddc57dafcaf1ca0d1dc35bee387a173
Abul Hossain <mdabulhossain780@gmail.com>
2023-04-05 00:11:03 +0600

    feat: added toast component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
+import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
@@ -14,6 +15,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Toaster position="bottom-center" />
       <SchemaProvider>
         <Head />
         <Component {...pageProps} />
